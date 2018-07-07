50ee96a5bfcebf055e14b0afa91e384a02e84b06
LINUX-PC <jesusrp98@outlook.com>
2018-07-07 12:46:28 +0200

    Done clean-file.sh

--- clean-file.sh
+++ clean-file.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # ** CLEAN FILE **
-#	Receive a .conf file from the /conf folder.
+#	Receive a .conf file from the ./conf folder.
 #	It cleans all comments (#) & all the blank lines
 #   Returns the cleaned file
 #   USE IT TO PROCESS .CONF FILES!
+
+cat $1 | grep -E '[^$]' | grep -vE '^[ ]*#'
--- is-file-empty.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# ** IS FILE EMPTY **
-#   Receive a file and returns if is empty or not
-#       - 0: file is not empty
-#       - 1: file is empty
