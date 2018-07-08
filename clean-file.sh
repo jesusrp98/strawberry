fb14ddd194b128fd74a578558b17083d355c966c
LINUX-PC <jesusrp98@outlook.com>
2018-07-08 14:03:59 +0200

    Done option managment on main

--- clean-file.sh
+++ clean-file.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # ** CLEAN FILE **
+
 #	Receive a .conf file from the ./conf folder.
 #	It cleans all comments (#) & all the blank lines
 #   Returns the cleaned file
--- setup.sh
+++ setup.sh
@@ -7,7 +7,42 @@
 #   -h: User is asking for help
 #		Help file is in ./help.conf
 #   -f: Path & file name for final script
-#		If path isn't specified, generate file in project root
+#       User must provide file path
+#	If path isn't specified, generate file in project root
 
-touch $1
-./modules/repos.sh $1 
+while getopts "hf:" OPTION; do
+    case $OPTION in
+    # Asking for help
+        h )
+            cat help.conf
+            exit 0
+            ;;
+    # Input a file path
+    # User must provide file path
+        f )
+            if [[ -e $OPTARG ]]; then
+                echo "existe"
+                exit 1
+            else
+                echo "no exite"
+                script=$OPTARG
+            fi
+            ;;
+        ? )
+            cat help.conf
+            exit 1
+            ;;
+    esac
+done
+
+# If no argument was written
+# Default file path
+if [ $OPTIND -eq 1 ]; then
+    script=$(date +"d%Ft%H-%M-%S-script").sh
+    echo "File path was not provided. Generating file in $script"
+fi
+
+shift $((OPTIND-1))
+
+# Creates the file
+touch $script
