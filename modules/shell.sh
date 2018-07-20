c37c715a4cfed7448ce47ab4c785e90771e2eb33
LINUX-PC <jesusrope98@outlook.com>
2018-07-20 18:05:36 +0200

    Fixed shell.sh

--- modules/shell.sh
+++ modules/shell.sh
@@ -12,17 +12,17 @@ if [ $# != 1 ];then
 fi
 
 #Asigno a una variable la(s) shells
-newShell=$(../clean-file.sh "../conf/shell.conf")
+newShell=$(. ./clean-file.sh ./conf/shell.conf)
 
 #Miro si solo hay una
-if [ $( (../clean-file.sh "../conf/shell.conf") | wc -l) != 1 ];then
+if [ $(echo "$newShell" | wc -l) != 1 ];then
 	echo "ERROR: Solo puede haber una shell en el shell.conf!"
 	exit 0
 fi
 
 #Compruebo que exista dicha shell
 found=0
-for x in $(../clean-file.sh "/etc/shells")
+for x in $newShell
 do
 	if [ $x == $newShell ];then
 		found=1
@@ -35,4 +35,4 @@ if [ "$found" != 1 ];then
 fi
 
 #Imprimo en el lugar especificado por paramtros el comando de chsh con el shell a usar
-echo "chsh -s "$newShell"" > "$1"
+echo "chsh -s "$newShell"" >> $1
