f873d9bef5d495c0badb2553cd23cb34be1f44b8
Miguel Ángel Ramírez Ruiz <33127182+Miguelo0098@users.noreply.github.com>
2018-07-22 14:06:17 +0200

    Update apt.sh

--- modules/apt.sh
+++ modules/apt.sh
@@ -4,3 +4,12 @@
 #	First, adds all repos specified by the user
 #		This is done calling ./modules/apt-repos
 #	Reads packages name from ./conf/apt.conf & installs them
+
+# Capture of packages in one single variable
+packages=$(cat ../conf/apt.conf | grep -E '^#' -v | grep -E '^$' -v)
+
+# iterations of sudo apt install of every single package
+for i in $packages; do
+	echo "sudo apt install $i"
+	sudo apt install $i
+done
